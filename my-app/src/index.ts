fef3e2b1f302e04e5275334df7d0f01aeb6dab35
pyNightborn <110329448+gabrielCharibPolls@users.noreply.github.com>
2023-02-13 14:54:42 +0100

    9.4 Express

---
+++ my-app/src/index.ts
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+app.listen(3000, () => {
+  console.log("Express server is running on http://localhost:3000");
+});
